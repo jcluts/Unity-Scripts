cf78cbb89864d4a1557da8e53dced2f7e8f246e4
jcluts <jcluts@gmail.com>
2022-05-30 10:52:17 -0500

    Add files via upload

--- Contour.cs
+++ Contour.cs
@@ -12,6 +12,7 @@ namespace Persona
 
         public GameObject[] clothing;
 
+        //Rate limiting until I can figure out how to optimize ApplyMorphs in DQS.
         public float updateRate = 1.0f;
 
         private SkinnedMeshRenderer skinnedMeshRenderer;
@@ -42,7 +43,7 @@ namespace Persona
             public float clampLow;
             public float clampHigh;
 
-            public BlendShapeDriver(string mn, string bn, string a, float t, float cl, float ch)
+            public BlendShapeDriver(string mn, string bn, string a, float t, float cl = 0, float ch = 100)
             {
                 boneName = bn;
                 axis = a;
@@ -55,72 +56,72 @@ namespace Persona
 
         public List<BlendShapeDriver> blendShapeDrivers = new List<BlendShapeDriver>()
             {
-                new BlendShapeDriver("pJCMPelvisFwd_25","pelvis","x",-25,0,100),
+                new BlendShapeDriver("pJCMPelvisFwd_25","pelvis","x",-25),
 
-                new BlendShapeDriver("pJCMAbdomenFwd_35","abdomenLower","x",35,0,100),
+                new BlendShapeDriver("pJCMAbdomenFwd_35","abdomenLower","x",35),
 
-                new BlendShapeDriver("pJCMAbdomen2Fwd_40","abdomenUpper","x",40,0,100),
-                new BlendShapeDriver("pJCMAbdomen2Side_24_L","abdomenUpper","z",-24,0,100),
-                new BlendShapeDriver("pJCMAbdomen2Side_24_R","abdomenUpper","z",24,0,100),
+                new BlendShapeDriver("pJCMAbdomen2Fwd_40","abdomenUpper","x",40),
+                new BlendShapeDriver("pJCMAbdomen2Side_24_L","abdomenUpper","z",-24),
+                new BlendShapeDriver("pJCMAbdomen2Side_24_R","abdomenUpper","z",24),
 
-                new BlendShapeDriver("pJCMChestFwd_35","chestLower","x",35,0,100),
-                new BlendShapeDriver("pJCMChestSide_20_L","chestLower","z",-20,0,100),
-                new BlendShapeDriver("pJCMChestSide_20_R","chestLower","z",20,0,100),
+                new BlendShapeDriver("pJCMChestFwd_35","chestLower","x",35),
+                new BlendShapeDriver("pJCMChestSide_20_L","chestLower","z",-20),
+                new BlendShapeDriver("pJCMChestSide_20_R","chestLower","z",20),
 
-                new BlendShapeDriver("pJCMNeckLowerSide_40_L","neckLower","z",-40,0,100),
-                new BlendShapeDriver("pJCMNeckLowerSide_40_R","neckLower","z",40,0,100),
-                new BlendShapeDriver("pJCMNeckTwist_22_L","neckLower","y",22,0,100),
-                new BlendShapeDriver("pJCMNeckTwist_22_R","neckLower","y",-22,0,100),
+                new BlendShapeDriver("pJCMNeckLowerSide_40_L","neckLower","z",-40),
+                new BlendShapeDriver("pJCMNeckLowerSide_40_R","neckLower","z",40),
+                new BlendShapeDriver("pJCMNeckTwist_22_L","neckLower","y",22),
+                new BlendShapeDriver("pJCMNeckTwist_22_R","neckLower","y",-22),
 
-                new BlendShapeDriver("pJCMNeckBack_27","neckUpper","x",-27,0,100),
-                new BlendShapeDriver("pJCMNeckFwd_35","neckUpper","x",35,0,100),
+                new BlendShapeDriver("pJCMNeckBack_27","neckUpper","x",-27),
+                new BlendShapeDriver("pJCMNeckFwd_35","neckUpper","x",35),
 
-                new BlendShapeDriver("pJCMHeadBack_27","head","x",-27,0,100),
-                new BlendShapeDriver("pJCMHeadFwd_25","head","x",25,0,100),
+                new BlendShapeDriver("pJCMHeadBack_27","head","x",-27),
+                new BlendShapeDriver("pJCMHeadFwd_25","head","x",25),
 
-                new BlendShapeDriver("pJCMCollarTwist_n30_L","lCollar","x",-30,0,100),
-                new BlendShapeDriver("pJCMCollarTwist_n30_R","rCollar","x",-30,0,100),
-                new BlendShapeDriver("pJCMCollarTwist_p30_L","lCollar","x",30,0,100),
-                new BlendShapeDriver("pJCMCollarTwist_p30_R","rCollar","x",30,0,100),
-                new BlendShapeDriver("pJCMCollarUp_55_L","lCollar","z",55,0,100),
-                new BlendShapeDriver("pJCMCollarUp_55_R","rCollar","z",-55,0,100),
+                new BlendShapeDriver("pJCMCollarTwist_n30_L","lCollar","x",-30),
+                new BlendShapeDriver("pJCMCollarTwist_n30_R","rCollar","x",-30),
+                new BlendShapeDriver("pJCMCollarTwist_p30_L","lCollar","x",30),
+                new BlendShapeDriver("pJCMCollarTwist_p30_R","rCollar","x",30),
+                new BlendShapeDriver("pJCMCollarUp_55_L","lCollar","z",55),
+                new BlendShapeDriver("pJCMCollarUp_55_R","rCollar","z",-55),
 
-                new BlendShapeDriver("pJCMShldrDown_40_L","lShldrBend","z",40,0,100),
-                new BlendShapeDriver("pJCMShldrDown_40_R","rShldrBend","z",-40,0,100),
-                new BlendShapeDriver("pJCMShldrFwd_110_L","lShldrBend","y",110,0,100),
-                new BlendShapeDriver("pJCMShldrFwd_110_R","rShldrBend","y",-110,0,100),
-                new BlendShapeDriver("pJCMShldrUp_90_L","lShldrBend","z",-90,0,100),
-                new BlendShapeDriver("pJCMShldrUp_90_R","rShldrBend","z",90,0,100),
+                new BlendShapeDriver("pJCMShldrDown_40_L","lShldrBend","z",40),
+                new BlendShapeDriver("pJCMShldrDown_40_R","rShldrBend","z",-40),
+                new BlendShapeDriver("pJCMShldrFwd_110_L","lShldrBend","y",110),
+                new BlendShapeDriver("pJCMShldrFwd_110_R","rShldrBend","y",-110),
+                new BlendShapeDriver("pJCMShldrUp_90_L","lShldrBend","z",-90),
+                new BlendShapeDriver("pJCMShldrUp_90_R","rShldrBend","z",90),
 
-                new BlendShapeDriver("pJCMForeArmFwd_135_L","lForearmBend","y",135,0,100),
-                new BlendShapeDriver("pJCMForeArmFwd_135_R","rForearmBend","y",-135,0,100),
-                new BlendShapeDriver("pJCMForeArmFwd_75_L","lForearmBend","y",75,0,100),
-                new BlendShapeDriver("pJCMForeArmFwd_75_R","rForearmBend","y",-75,0,100),
+                new BlendShapeDriver("pJCMForeArmFwd_135_L","lForearmBend","y",135),
+                new BlendShapeDriver("pJCMForeArmFwd_135_R","rForearmBend","y",-135),
+                new BlendShapeDriver("pJCMForeArmFwd_75_L","lForearmBend","y",75),
+                new BlendShapeDriver("pJCMForeArmFwd_75_R","rForearmBend","y",-75),
 
-                new BlendShapeDriver("pJCMHandDwn_70_L","lHand","z",-70,0,100),
-                new BlendShapeDriver("pJCMHandDwn_70_R","rHand","z",70,0,100),
-                new BlendShapeDriver("pJCMHandUp_80_L","lHand","z",80,0,100),
-                new BlendShapeDriver("pJCMHandUp_80_R","rHand","z",-80,0,100),
+                new BlendShapeDriver("pJCMHandDwn_70_L","lHand","z",-70),
+                new BlendShapeDriver("pJCMHandDwn_70_R","rHand","z",70),
+                new BlendShapeDriver("pJCMHandUp_80_L","lHand","z",80),
+                new BlendShapeDriver("pJCMHandUp_80_R","rHand","z",-80),
 
 
-                new BlendShapeDriver("pJCMThighBack_35_L","lThighBend","x",35,0,100),
-                new BlendShapeDriver("pJCMThighBack_35_R","rThighBend","x",35,0,100),
-                new BlendShapeDriver("pJCMThighFwd_115_L","lThighBend","x",-115,0,100),
-                new BlendShapeDriver("pJCMThighFwd_115_R","rThighBend","x",-115,0,100),
-                new BlendShapeDriver("pJCMThighFwd_57_L","lThighBend","x",-57,0,100),
-                new BlendShapeDriver("pJCMThighFwd_57_R","rThighBend","x",-57,0,100),
-                new BlendShapeDriver("pJCMThighSide_85_L","lThighBend","z",-85,0,100),
-                new BlendShapeDriver("pJCMThighSide_85_R","rThighBend","z",85,0,100),
+                new BlendShapeDriver("pJCMThighBack_35_L","lThighBend","x",35),
+                new BlendShapeDriver("pJCMThighBack_35_R","rThighBend","x",35),
+                new BlendShapeDriver("pJCMThighFwd_115_L","lThighBend","x",-115),
+                new BlendShapeDriver("pJCMThighFwd_115_R","rThighBend","x",-115),
+                new BlendShapeDriver("pJCMThighFwd_57_L","lThighBend","x",-57),
+                new BlendShapeDriver("pJCMThighFwd_57_R","rThighBend","x",-57),
+                new BlendShapeDriver("pJCMThighSide_85_L","lThighBend","z",-85),
+                new BlendShapeDriver("pJCMThighSide_85_R","rThighBend","z",85),
 
-                new BlendShapeDriver("pJCMShinBend_155_L","lShin","x",155,0,100),
-                new BlendShapeDriver("pJCMShinBend_155_R","rShin","x",155,0,100),
-                new BlendShapeDriver("pJCMShinBend_90_L","lShin","x",90,0,100),
-                new BlendShapeDriver("pJCMShinBend_90_R","rShin","x",90,0,100),
+                new BlendShapeDriver("pJCMShinBend_155_L","lShin","x",155),
+                new BlendShapeDriver("pJCMShinBend_155_R","rShin","x",155),
+                new BlendShapeDriver("pJCMShinBend_90_L","lShin","x",90),
+                new BlendShapeDriver("pJCMShinBend_90_R","rShin","x",90),
 
-                new BlendShapeDriver("pJCMFootDwn_75_L","lFoot","x",75,0,100),
-                new BlendShapeDriver("pJCMFootDwn_75_R","rFoot","x",75,0,100),
-                new BlendShapeDriver("pJCMFootUp_40_L","lFoot","x",-40,0,100),
-                new BlendShapeDriver("pJCMFootUp_40_R","rFoot","x",-40,0,100),
+                new BlendShapeDriver("pJCMFootDwn_75_L","lFoot","x",75),
+                new BlendShapeDriver("pJCMFootDwn_75_R","rFoot","x",75),
+                new BlendShapeDriver("pJCMFootUp_40_L","lFoot","x",-40),
+                new BlendShapeDriver("pJCMFootUp_40_R","rFoot","x",-40),
 
                 //new BlendShapeDriver("pJCMToesUp_60_L","lToe","x",-60,0,100),
                 //new BlendShapeDriver("pJCMToesUp_60_R","rToe","x",-60,0,100),
@@ -171,7 +172,7 @@ namespace Persona
         {
             dualQuaternionSkinner.SetBlendShapeWeights(morphWeights);
 
-            //Assumes clothing has same collection of blendshapes, in same order. Shold be the case when sing the Daz bridge.
+            //Assumes clothing has same collection of blendshapes, in same order. Should be the case when sing the Daz bridge.
             foreach (var clothingItem in clothing)
             {
                 var clothingSkinner = clothingItem.GetComponent<DualQuaternionSkinner>();
@@ -190,7 +191,7 @@ namespace Persona
 
             percent = Mathf.Clamp(percent, driver.clampLow, driver.clampHigh);
 
-            //noise reduction
+            //Noise reduction.
             if (percent < 10)
             {
                 percent = 0;
